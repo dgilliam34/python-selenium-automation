00376870383cf691a4b2dd89db83d53365a7b77e
dgilliam34 <d.gilliamm26@gmail.com>
2025-09-27 17:20:28 -0400

    target_test.py

--- sample_script.py
+++ sample_script.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 
+from features.steps.product_search import click_search_icon
+
 # get the path to the ChromeDriver executable
 driver_path = ChromeDriverManager().install()
 
@@ -13,21 +15,11 @@ driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
 # open the url
-driver.get('https://www.google.com/')
+driver.get("https://www.target.com/")
 
-# populate search field
-search = driver.find_element(By.NAME, 'q')
-search.clear()
-search.send_keys('Car')
+driver.find_element(By.XPATH,"//span[contains(.,'Account')]").click()
+driver.find_element(By.XPATH, "//button[@data-test='accountNav-signIn']").click()
+sleep(5)
+driver.find_element(By.XPATH,"//h1[contains(.,'Sign in or create account')]")
+driver.find_element(By.ID,"login")
 
-# wait for 4 sec
-sleep(4)
-
-# click search button
-driver.find_element(By.NAME, 'btnK').click()
-
-# verify search results
-assert 'car'.lower() in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
-print('Test Passed')
-
-driver.quit()
